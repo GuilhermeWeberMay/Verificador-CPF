c90116c181fb4db02baea6c1d6b33aaea9291652
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-08-15 10:54:33 -0300

    Feito: Entrada do CPF, se são 11 caracteres e se são 11 números

--- src/Verificador_CPF.java
+++ src/Verificador_CPF.java
@@ -5,12 +5,28 @@ public class Verificador_CPF {
         Scanner read = new Scanner(System.in);
         // Declaração da variavel
         String cpfEntrada;
-        int qtdCaracterCpf;
+        int qtdCaracterCpf, qtdNumeroCpf = 0;
         // Entrada de dados
         System.out.print("Somente números \nFavor informe seu CPF: ");
         cpfEntrada =read.next();
         // Verificação de quantidade de carasteres informados pelo usuario
         qtdCaracterCpf = cpfEntrada.length();
-
+        // Verificação - se não tiver 11 digitos não é um cpf valido
+        if (qtdCaracterCpf == 11) {
+            // Verificação - para confirmar que os 11 digitos são números
+            for (int i = 0; i < 11; i++){
+                if (Character.isDigit(cpfEntrada.charAt(i))){
+                    qtdNumeroCpf++;
+                }
+            }
+            // Se não tiver 11 números CPF inválido
+            if (qtdNumeroCpf != 11) {
+                System.out.println("CPF invalido");
+            }
+        }
+        else {
+            System.out.println("CPF invalido");
+        }
+        System.out.print(qtdNumeroCpf);
     }
 }
